da3ae264868ad10d343bae7455da17f3ac36a83b
Jelle Callewaert <Jelle.Callewaert@realdolmen.com>
2019-02-21 10:56:19 +0100

    added models

---
+++ streekbieren-avond-backend/Models/Brouwerij.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace streekbieren_avond_backend.Models
+{
+    public class Brouwerij
+    {
+        public string Naam { get; set; }
+        public string Locatie { get; set; }
+        public DateTime datumOpgericht { get; set; }
+    }
+}
---
+++ streekbieren-avond-backend/Models/Streekbier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace streekbieren_avond_backend.Models
+{
+    public class Streekbier
+    {
+        public string Naam { get; set; }
+        public double Percentage { get; set; }
+        public Brouwerij Brouwerij { get; set; }
+    }
+}
